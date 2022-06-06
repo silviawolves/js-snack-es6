8bef70367ef39546e3ee41e612abaad5193ac405
silviawolves <silviacanepa.w@gmail.com>
2022-06-06 13:27:06 +0200

    codice base + consegna

---
+++ snack1/snack1-script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+
+
+
+
+
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/
