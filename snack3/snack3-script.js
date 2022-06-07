19417a87e733c95b623cc061670ea6ac2b801b0e
silviawolves <silviacanepa.w@gmail.com>
2022-06-07 13:41:30 +0200

    codice base + consegna

---
+++ snack3/snack3-script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+
+
+
+/*
+Dato l’array di nomi:
+const myArray = [‘Michele’, ‘Fabio’, ‘Roberto’, ‘Giovanni’, ‘Simone’, ‘Chiara’];
+e dati due numeri min e max (min più piccolo di max).
+Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+Eseguiamo questo esercizio prima con forEach e poi con filter.
+*/
